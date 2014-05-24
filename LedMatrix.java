64c71e1e061a6b7450d734528fe14979813cc20b
mrrs6 <jdso@hotmail.com>
2014-05-24 19:24:37 +0100

    Implemented run() function, added Timer class and functions to shift columns of a line.

--- LedMatrix.java
+++ LedMatrix.java
@@ -20,7 +20,12 @@ class LedMatrix implements Runnable {
   final int SPI_RATE = 10000000;
   final GpioPinDigitalOutput OE;
 
+  Timer timer;
+  final int SHIFTTIME_MS = 250;
+  
   LedMatrix() {
+	timer = new Timer(); 
+  
     // Initializing message queue.
     messages = new LinkedList<String>();
 
@@ -42,17 +47,78 @@ class LedMatrix implements Runnable {
   }
 
   void run() {
-    OE.high();
-    while(message.size() == 0) {
-      sleep(5);
-    }
-
-
-
-    OE.low();
+	String message;
+	while(1)
+	{
+		OE.high();
+		
+		while(messages.size() == 0) {
+			sleep(5);
+		}
+	
+		s = new String( message.top() + ' ' ); 	// added whitespace at end of message to have something to show when shifting to last character.
+		for(int i=0; i<s.length()-1; i++)		// s.length()-1 because we added a whitespace
+		{
+			timer.tic();
+			for(int shift = 0; shift<8; shift++)
+			{
+				while(timer.toc_ms() < SHIFTTIME_MS)
+				{
+					for(int j=0; j<8; j++)
+					{
+							OE.high();
+							Spi.wiringPiSPIDataRW(CHANNEL, shift2C( font[s.charAt(i)] , font[s.charAt(i+1)] , shift)[j] , 2);
+							OE.low();
+					}
+				}
+			}
+		}
+	}
   }
+  
+	byte[] shift2C(byte[] char1, byte[] char2, shiftAmount)
+	{  
+		byte[] char1ShiftedLeft = new byte[8];
+		byte[] char2ShiftedRight = new byte[8];
+		byte[] result = new byte[8];
+		
+		// shift char1 lines 1 column left
+		for(int i=0; i<8; i++)
+		{
+			if((byte)(char1[i]<<1) >= -128)
+				char1ShiftedLeft[i] = (byte)(char1[i]<<1);
+			else
+				char1ShiftedLeft[i] = (byte)0;
+				
+			if( (byte)(char2[i]>>(8-i)) >=0 )
+				char2ShiftedRight[i] = (byte)(char2[i]>>(8-i))
+			else
+				char2ShiftedRight[i] = (byte)0;
+				
+			result[i] = char1ShiftedLeft[i] | char2ShiftedRight[i];
+		}
+		
+		return result;
+	}
+	
+	void addNewMessage(String message)
+	{
+		messages.add(message);
+	}
+	
+}
 
-  void addNewMessage(String message) {
-    messages.add(message);
-  }
+public class Timer
+{
+	private long time_ms;
+
+	void tic()
+	{
+		this.time_ms = System.currentTimeMillis();
+	}
+  
+	int toc_ms()
+	{
+		return (int)( this.time_ms - System.currrentTimeMillis() );
+	}
 }
